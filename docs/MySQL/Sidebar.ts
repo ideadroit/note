74e85312fd9970c0aa2d48db5bab03c2feabb0f2
idea droit <ideadroit@qq.com>
2022-11-14 03:37:53 +0800

    init

---
+++ docs/MySQL/Sidebar.ts
@@ -0,0 +1,13 @@
+import { SidebarConfigArray } from 'vuepress'
+
+const MySQLSidebar: SidebarConfigArray = [
+    '/MySQL/index.md',
+    '/MySQL/DDL.md',
+    '/MySQL/DML.md',
+    '/MySQL/DQL.md',
+    '/MySQL/DCL.md',
+    '/MySQL/Other.md'
+
+]
+
+export { MySQLSidebar }
